3ab8fe7bdd954b1dfffccdefae077edbf01bffca
Adam Evans <primeae@amazon.com>
2022-10-19 11:56:36 +0100

    Added cells of data using TableDelegate, DataSource and dequeuingReusableCell

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -20,6 +20,9 @@ class HomeViewController: UIViewController {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         view.addSubview(homeFeedTable)
+        
+        homeFeedTable.delegate = self
+        homeFeedTable.dataSource = self
     }
     
     override func viewDidLayoutSubviews() {
@@ -27,4 +30,18 @@ class HomeViewController: UIViewController {
         homeFeedTable.frame = view.bounds
     }
 
+}
+
+//Using extension to keep code clean
+extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20   //Setting to 20 rows
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "Hello World"    //For testing
+        return cell
+    }
 }
