1f753272b6ac98225e190f791c39ac4d824c1d2e
Adam Evans <primeae@amazon.com>
2022-10-19 11:49:08 +0100

    Adding table view but need to populate with data to dequeue cells with data

--- Netflix Clone/Controllers/HomeViewController.swift
+++ Netflix Clone/Controllers/HomeViewController.swift
@@ -8,11 +8,23 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private let homeFeedTable: UITableView = {
+        
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }() //<- Anonymous Closure
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         view.backgroundColor = .systemBackground
+        view.addSubview(homeFeedTable)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        homeFeedTable.frame = view.bounds
     }
 
 }
