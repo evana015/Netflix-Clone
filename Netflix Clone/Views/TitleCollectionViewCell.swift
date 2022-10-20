caf25bc4669bbf9d2fac4ade17e6bf5e880ef2c0
Adam Evans <primeae@amazon.com>
2022-10-20 17:47:09 +0100

    Creating essential functions for TitleCollectionViewCell and posterImageView

--- Netflix Clone/Views/TitleCollectionViewCell.swift
+++ Netflix Clone/Views/TitleCollectionViewCell.swift
@@ -9,4 +9,25 @@ import UIKit
 
 class TitleCollectionViewCell: UICollectionViewCell {
     
+    static let identifier = "TitleCollectionViewCell"
+    
+    private let posterImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        contentView.addSubview(posterImageView)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        posterImageView.frame = contentView.bounds
+    }
 }
