1a9c44cabad3d188a1b4a847e2d636b6b9f5e19f
Adam Evans <primeae@amazon.com>
2022-10-20 14:29:28 +0100

    Adding APICaller and creating a constant API_KEY

---
+++ Netflix Clone/Managers/APICaller.swift
@@ -0,0 +1,16 @@
+//
+//  APICaller.swift
+//  Netflix Clone
+//
+//  Created by Evans, Adam on 20/10/2022.
+//
+
+import Foundation
+
+struct Constants {
+    static let API_KEY = "7502a13c57cecf92823b6e9f7424ef17"
+}
+
+class APICaller {
+    static let shared = APICaller()
+}
