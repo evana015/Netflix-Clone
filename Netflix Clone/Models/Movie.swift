09ca45f9343c53897474e0ba52fd152f84558d92
Adam Evans <primeae@amazon.com>
2022-10-20 15:59:27 +0100

    Adding Movie Model

---
+++ Netflix Clone/Models/Movie.swift
@@ -0,0 +1,31 @@
+//
+//  Movie.swift
+//  Netflix Clone
+//
+//  Created by Evans, Adam on 20/10/2022.
+//
+
+import Foundation
+
+struct TrendingMoviesResponse {
+    let results: [Movie]
+}
+
+struct Movie {
+
+}
+
+/* EXAMPLE DATA
+ id = 616820;
+ "media_type" = movie;
+ "original_language" = en;
+ "original_title" = "Halloween Ends";
+ overview = "Four years after the events of Halloween in 2018, Laurie has decided to liberate herself from fear and rage and embrace life. But when a young man is accused of killing a boy he was babysitting, it ignites a cascade of violence and terror that will force Laurie to finally confront the evil she can\U2019t control, once and for all.";
+ popularity = "4845.496";
+ "poster_path" = "/3uDwqxbr0j34rJVJMOW6o8Upw5W.jpg";
+ "release_date" = "2022-10-12";
+ title = "Halloween Ends";
+ video = 0;
+ "vote_average" = "6.86";
+ "vote_count" = 433;
+ */
