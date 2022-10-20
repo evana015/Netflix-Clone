786735c29eddb973f79577cafddb561825103e9d
Adam Evans <primeae@amazon.com>
2022-10-20 16:40:49 +0100

    Making API Call only movies

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -20,7 +20,7 @@ class APICaller {
     static let shared = APICaller()
     
     func getTrendingMovies(compeltion: @escaping (Result<[Movie], Error>) -> Void) {
-        guard let url = URL(string: "\(Constants.baseURL)/3/trending/all/day?api_key=\(Constants.API_KEY)") else {return}
+        guard let url = URL(string: "\(Constants.baseURL)/3/trending/movie/day?api_key=\(Constants.API_KEY)") else {return}
         
         //Get data
         let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
