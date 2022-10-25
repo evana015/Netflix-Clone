30702eb0622dc7cc59c745aa762861990510f607
Adam Evans <primeae@amazon.com>
2022-10-25 13:11:23 +0100

    Adding button to the cell

--- Netflix Clone/Views/TitleTableViewCell.swift
+++ Netflix Clone/Views/TitleTableViewCell.swift
@@ -13,7 +13,10 @@ class TitleTableViewCell: UITableViewCell {
     
     private let playTitleButton: UIButton = {
         let button = UIButton()
+        let image = UIImage(systemName: "play.circle", withConfiguration: UIImage.SymbolConfiguration(pointSize: 30))
+        button.setImage(image, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .white
         return button
     }()
     
@@ -52,8 +55,14 @@ class TitleTableViewCell: UITableViewCell {
             titlesLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
         ]
         
+        let playTitleButtonConstraints = [
+            playTitleButton.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+            playTitleButton.centerYAnchor.constraint(equalTo: contentView.centerYAnchor)
+        ]
+        
         NSLayoutConstraint.activate(titlesPosterUIImageViewConstraints)
         NSLayoutConstraint.activate(titlesLabelConstraints)
+        NSLayoutConstraint.activate(playTitleButtonConstraints)
     }
     
     public func configure(with model: TitleViewModel) {
