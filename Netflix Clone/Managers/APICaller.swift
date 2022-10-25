9854f50956bba3b593bdf2444e29819000919a1f
Adam Evans <primeae@amazon.com>
2022-10-25 17:08:21 +0100

    Fixed URL in APICaller

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -172,7 +172,7 @@ class APICaller {
         guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
             return
         }
-        guard let url = URL(string: "\(Constants.YouTubeBaseURL)q=\(query)key=\(Constants.YouTubeAPI_KEY)") else {
+        guard let url = URL(string: "\(Constants.YouTubeBaseURL)q=\(query)&key=\(Constants.YouTubeAPI_KEY)") else {
             return
         }
         //Get data
