40ab66f4fcb85f94a31e494efc683d59bc81039c
Adam Evans <primeae@amazon.com>
2022-10-25 15:13:25 +0100

    Adding a title to SearchVC

--- Netflix Clone/Controllers/SearchViewController.swift
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -8,12 +8,14 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemBackground
-    }
+        title = "Search"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always    }
     
 
 }
