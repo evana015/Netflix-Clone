59a0654be137c099d20947059e01eaa7c7698e58
Adam Evans <primeae@amazon.com>
2022-10-25 17:06:57 +0100

    Adding Basic YouTube API call and testing it with string "Harry potter"

--- Netflix Clone/Managers/APICaller.swift
+++ Netflix Clone/Managers/APICaller.swift
@@ -10,6 +10,8 @@ import Foundation
 struct Constants {
     static let API_KEY = "7502a13c57cecf92823b6e9f7424ef17"
     static let baseURL = "https://api.themoviedb.org"
+    static let YouTubeAPI_KEY = "AIzaSyCPFFIzZLgUPEw_Kh1SvKvxFJ5lENdAnXQ"
+    static let YouTubeBaseURL = "https://youtube.googleapis.com/youtube/v3/search?"
 }
 
 enum APIError: Error {
@@ -161,6 +163,30 @@ class APICaller {
                 compeltion(.success(results.results))
             } catch {
                 compeltion(.failure(APIError.failedToGetData))
+            }
+        }
+        task.resume()
+    }
+    
+    func getMovie(with query: String) {
+        guard let query = query.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed) else {
+            return
+        }
+        guard let url = URL(string: "\(Constants.YouTubeBaseURL)q=\(query)key=\(Constants.YouTubeAPI_KEY)") else {
+            return
+        }
+        //Get data
+        let task = URLSession.shared.dataTask(with: URLRequest(url: url)) { data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            //Convert to JSON Object
+            do {
+                let results = try JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                print(results)
+            } catch {
+                print(error.localizedDescription)
             }
         }
         task.resume()
