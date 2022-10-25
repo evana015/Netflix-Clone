0d673e0946ddc1b57407d0ad4d125147d6f8a9e2
Adam Evans <primeae@amazon.com>
2022-10-25 15:33:40 +0100

    Adding SearchResultsVC

---
+++ Netflix Clone/Controllers/SearchResultsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResultsViewController.swift
+//  Netflix Clone
+//
+//  Created by Evans, Adam on 25/10/2022.
+//
+
+import UIKit
+
+class SearchResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+}
