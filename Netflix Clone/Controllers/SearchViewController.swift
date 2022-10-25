d9007b1e2492ca073dd4dfbf10fbfd6de7dc9bd3
Adam Evans <primeae@amazon.com>
2022-10-25 15:16:22 +0100

    Adding table to searchVC

--- Netflix Clone/Controllers/SearchViewController.swift
+++ Netflix Clone/Controllers/SearchViewController.swift
@@ -9,13 +9,24 @@ import UIKit
 
 class SearchViewController: UIViewController {
     
-    
+    private let discoverTable: UITableView = {
+        let table = UITableView()
+        table.register(TitleTableViewCell.self, forCellReuseIdentifier: TitleTableViewCell.identifier)
+        return table
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Search"
         navigationController?.navigationBar.prefersLargeTitles = true
-        navigationController?.navigationItem.largeTitleDisplayMode = .always    }
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(discoverTable)
+    }
+        
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        discoverTable.frame = view.bounds
+    }
     
-
 }
