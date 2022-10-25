84f3a8df730ad643b95a66369e22d70fd6bbd70f
Adam Evans <primeae@amazon.com>
2022-10-25 13:04:03 +0100

    Fixing cell sizing

--- Netflix Clone/Views/TitleTableViewCell.swift
+++ Netflix Clone/Views/TitleTableViewCell.swift
@@ -57,8 +57,9 @@ class TitleTableViewCell: UITableViewCell {
     }
     
     public func configure(with model: TitleViewModel) {
-        guard let url = URL(string: "https://image.tmdb.org/t/p/w500/\(model.posterURL)") else {return}
-    
+        guard let url = URL(string: "https://image.tmdb.org/t/p/w500/\(model.posterURL)") else {
+            return
+        }
         titlesPosterUIImageView.sd_setImage(with: url, completed: nil)
         titlesLabel.text = model.titleName
     }
