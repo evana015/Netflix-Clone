c0d2752e74042e38b2317df7a18af72a48b61cb6
Adam Evans <primeae@amazon.com>
2022-10-25 12:04:44 +0100

    Added constraints and playButton

--- Netflix Clone/Views/TitleTableViewCell.swift
+++ Netflix Clone/Views/TitleTableViewCell.swift
@@ -11,14 +11,20 @@ class TitleTableViewCell: UITableViewCell {
     
     static let identifier = "TitleTableViewCell"
     
-    private let titleLabel: UILabel = {
-       let label = UILabel()
+    private let playTitleButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
+    private let titlesLabel: UILabel = {
+        let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
         return label
     }()
     
     private let titlesPosterUIImageView: UIImageView = {
-       let imageView = UIImageView()
+        let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFill
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
@@ -27,8 +33,24 @@ class TitleTableViewCell: UITableViewCell {
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         contentView.addSubview(titlesPosterUIImageView)
-        contentView.addSubview(titleLabel)
+        contentView.addSubview(titlesLabel)
+        contentView.addSubview(playTitleButton)
+        
+        applyConstraints()
     }
+    
+    private func applyConstraints() {
+        let titlesPosterUIImageViewConstraints = [
+            titlesPosterUIImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            titlesPosterUIImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 15),
+            titlesPosterUIImageView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -15),
+            titlesPosterUIImageView.widthAnchor.constraint(equalToConstant: 100)
+        ]
+        
+        NSLayoutConstraint.activate(titlesPosterUIImageViewConstraints)
+    }
+    
+
     
     required init?(coder: NSCoder) {
         fatalError()
