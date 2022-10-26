a936083f78b4e19de253af59ded163d2dcb40090
Adam Evans <primeae@amazon.com>
2022-10-26 16:27:37 +0100

    Adding TitlePreviewViewController

---
+++ Netflix Clone/Controllers/General/TitlePreviewViewController.swift
@@ -0,0 +1,18 @@
+//
+//  TitlePreviewViewController.swift
+//  Netflix Clone
+//
+//  Created by Evans, Adam on 26/10/2022.
+//
+
+import UIKit
+
+class TitlePreviewViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
