25959a761b9017ead1a3537607565ebea58f5187
Adam Evans <primeae@amazon.com>
2022-11-02 15:31:35 +0000

    Creating context and a model in which to be saved to NetflixCloneModel data model

--- Netflix Clone/Managers/DataPersistenceManager.swift
+++ Netflix Clone/Managers/DataPersistenceManager.swift
@@ -6,12 +6,42 @@
 //
 
 import Foundation
+import UIKit
+import CoreData
 
 class DataPersistenceManager {
     
+    enum DataBaseError: Error {
+        case failedToSaveData
+    }
+    
     static let shared = DataPersistenceManager()
     
-    func downloadTitleWith(Model: Title, completion: @escaping (Result <Void, Error>) -> Void) {
+    func downloadTitleWith(model: Title, completion: @escaping (Result <Void, Error>) -> Void) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
         
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let item = TitleItem(context: context)
+        
+        item.id = Int64(model.id)
+        item.original_title = model.original_title
+        item.original_name = model.original_name
+        item.overview = model.overview
+        item.media_type = model.media_type
+        item.poster_path = model.poster_path
+        item.release_date = model.release_date
+        item.vote_count = Int64(model.vote_count)
+        item.vote_average = model.vote_average
+        
+        do {
+            try context.save()
+            completion(.success(()))    // Parsing () is used to parse void
+        } catch {
+            print(error.localizedDescription)
+            completion(.failure(DataBaseError.failedToSaveData))
+        }
     }
 }
--- Netflix Clone/Models/Title.swift
+++ Netflix Clone/Models/Title.swift
@@ -12,15 +12,15 @@ struct TrendingTitleResponse: Codable {
 }
 
 struct Title: Codable {
-    let id: Int?
+    let id: Int
     let media_type: String?
     let original_name: String?
     let original_title: String?
     let poster_path: String?
     let overview: String?
-    let vote_count: Int?
+    let vote_count: Int
     let release_date: String?
-    let vote_average: Double?
+    let vote_average: Double
 }
 
 /* EXAMPLE DATA
